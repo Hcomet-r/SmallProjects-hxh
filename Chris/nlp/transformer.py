fc9747470ba57cc0d903d154f354fb497e20b789
zhang-chris0320 <zhhengjia2025@lzu.edu.cn>
2026-02-10 20:21:03 +0800

    加了transformer

--- Chris/nlp/transformer.py
+++ Chris/nlp/transformer.py
@@ -4,6 +4,7 @@ import torch.nn.functional as F
 import math
 import torch.nn as nn
 from torch import tensor
+
 class TokenEmbedding(nn.Embedding):
     def __init__(self, vocb_size,embedding_dim):   #两个参数，一个词汇表大小，一个embedding维度
         super(TokenEmbedding,self).__init__(vocb_size,embedding_dim,padding_idx=1)
@@ -187,6 +188,51 @@ class Decoder(nn.Module):
         for layer in self.layers:
             dec=layer(dec,enc,t_mask,s_mask)
         dec=self.fc(dec)
-        return dec                        
+        return dec    
+class Transformer(nn.Module):
+    def __init__(self, 
+                src_pad_ix,
+                trg_pad_ix,
+                enc_voc_size,
+                dec_voc_size,
+                embedding_dim,
+                max_len,
+                n_heads ,
+                ffn_hidden,
+                n_layers,
+                drop_prob,
+                device 
+                 ):
+        super(Transformer,self).__init__()
+        self.encoder=Encoder(
 
-  
+            enc_voc_size,
+            max_len,
+            embedding_dim,
+            ffn_hidden,
+            n_heads,
+            n_layers,
+            drop_prob,
+            device,
+        )
+        self.decoder=Decoder(dec_voc_size,max_len,embedding_dim,ffn_hidden,n_heads,n_layers,drop_prob,device)
+        self.src_pad_idx=src_pad_ix
+        self.trg_pad_idx=trg_pad_ix
+        self.device=device
+    def make_pad_mask(self,q,k,pad_idx_q,pad_idx_k):
+        len_q,len_k=q.size(),k.size(1)
+        q=q.ne(pad_idx_q).unsqueeze(1).unsqueeze(3)
+        q=q.repeat(1,1,1,len_k)
+        k=k.ne(pad_idx_k).unsqueeze(1).unsqueeze(2)
+        k=k.repeat(1,1,len_q,1)
+        mask=q&k
+        return mask
+    def make_casual_mask(self,q,k):
+        mask=torch.tril(torch.ones(q,k)).type(torch.BoolTensor).to(self.device)
+        return mask
+    def forward(self,src,trg):
+        src_mask=self.make_pad_mask(src,src,self.src_pad_idx,self.src_pad_idx)
+        trg_mask=self.make_pad_mask(trg,trg,self.trg_pad_idx,self.trg_pad_idx)*self.make_casual_mask(trg,trg)
+        enc=self.encoder(src,src_mask)
+        out=self.decoder(trg,src,trg_mask,src_mask)
+        return out
